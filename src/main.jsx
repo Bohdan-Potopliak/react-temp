62f2ee172db205923734bb138a869b61552dd7a8
Bohdan Potopliak <bohdanpotopliak@gmail.com>
2025-06-13 21:19:15 +0300

    upd structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "modern-normalize";
-import "./index.css";
-import App from "./components/App.jsx";
+import 'modern-normalize';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { store, persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Router>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
+        </Router>
+        <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
+    </StrictMode>
 );
